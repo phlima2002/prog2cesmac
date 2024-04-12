4f9da373db8bf28c6a27c061cd28c0fe8bc8aa9a
Pedro Henrique <phlima2002@hotmail.com>
2024-04-12 10:51:21 -0300

    try and except adicionados e melhoria nos erros

--- projetoAV02/utils/cadastrarProduto.py
+++ projetoAV02/utils/cadastrarProduto.py
@@ -1,18 +1,25 @@
 produtosCadastrados = []
 def cadastrarProduto(): 
-
     nomeProduto = input('Insira o nome do produto que você deseja cadastrar\n')
-    qtdProduto = int(input('Insira quantas unidades você deseja comprar\n'))
 
-    produto = {
-        'nome' : nomeProduto.title(),
-        'qtd' : qtdProduto
-    }
+    try:
+        qtdProduto = int(input('Insira quantas unidades você deseja comprar\n'))
+        if(qtdProduto > 0):
+            produto = {
+            'nome' : nomeProduto.title(),
+            'qtd' : qtdProduto
+            }
+            produtosCadastrados.append(produto)
 
-    produtosCadastrados.append(produto)
+            print('Produto cadastrado com sucesso')
+        else: print('Insira uma quantidade maior que 0')
+    except:
+        print('Quantidade inválida. Por favor, insira um número inteiro.')
 
 
-    print('Produto cadastrado com sucesso')
+   
+
+
 
     
 
